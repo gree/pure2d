8f6d9ac7ea80ca50f77b0be4f43cc986015d2178
Long Ngo <long@funzio.com>
2012-10-24 17:41:21 -0700

    fixed length

--- pure2D/src/com/funzio/pure2D/text/TextObject.java
+++ pure2D/src/com/funzio/pure2D/text/TextObject.java
@@ -27,12 +27,14 @@ public class TextObject extends Rectangular {
             mTexture = null;
         }
 
-        Scene scene = getScene();
-        if (scene != null) {
-            setTexture(scene.getTextureManager().createTextTexture(mText, mOptions));
-            // match the size with the texture
-            if (mTexture != null) {
-                setSize(mTexture.getSize());
+        if (mText.length() > 0) {
+            Scene scene = getScene();
+            if (scene != null) {
+                setTexture(scene.getTextureManager().createTextTexture(mText, mOptions));
+                // match the size with the texture
+                if (mTexture != null) {
+                    setSize(mTexture.getSize());
+                }
             }
         }
     }
@@ -60,7 +62,7 @@ public class TextObject extends Rectangular {
         super.onAdded(parent);
 
         // if there is no texture yet
-        if (mTexture == null) {
+        if (mTexture == null && mText.length() > 0) {
             Scene scene = getScene();
             if (scene != null) {
                 setTexture(scene.getTextureManager().createTextTexture(mText, mOptions));
