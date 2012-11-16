da75c20f9cdd5dca9b422d429cb0e9c9799739b9
Long Ngo <long@funzio.com>
2012-11-15 17:39:02 -0800

    renaming

--- pure2D/src/com/funzio/pure2D/animators/ScaleAnimator.java
+++ pure2D/src/com/funzio/pure2D/animators/ScaleAnimator.java
@@ -10,49 +10,49 @@ import android.view.animation.Interpolator;
  * @author long
  */
 public class ScaleAnimator extends TweenAnimator {
-    protected float mSrcX = 0;
-    protected float mSrcY = 0;
-    protected float mDstX = 0;
-    protected float mDstY = 0;
+    protected float mScaleX1 = 0;
+    protected float mScaleY1 = 0;
+    protected float mScaleX2 = 0;
+    protected float mScaleY2 = 0;
     protected PointF mDelta = new PointF();
 
     public ScaleAnimator(final Interpolator interpolator) {
         super(interpolator);
     }
 
-    public void setValues(final float srcX, final float srcY, final float dstX, final float dstY) {
-        mSrcX = srcX;
-        mSrcY = srcY;
-        mDstX = dstX;
-        mDstY = dstY;
+    public void setValues(final float scaleX1, final float scaleY1, final float scaleX2, final float scaleY2) {
+        mScaleX1 = scaleX1;
+        mScaleY1 = scaleY1;
+        mScaleX2 = scaleX2;
+        mScaleY2 = scaleY2;
 
-        mDelta.x = dstX - srcX;
-        mDelta.y = dstY - srcY;
+        mDelta.x = scaleX2 - scaleX1;
+        mDelta.y = scaleY2 - scaleY1;
     }
 
-    public void start(final float srcX, final float srcY, final float dstX, final float dstY) {
-        mSrcX = srcX;
-        mSrcY = srcY;
-        mDstX = dstX;
-        mDstY = dstY;
+    public void start(final float scaleX1, final float scaleY1, final float scaleX2, final float scaleY2) {
+        mScaleX1 = scaleX1;
+        mScaleY1 = scaleY1;
+        mScaleX2 = scaleX2;
+        mScaleY2 = scaleY2;
 
-        mDelta.x = dstX - srcX;
-        mDelta.y = dstY - srcY;
+        mDelta.x = scaleX2 - scaleX1;
+        mDelta.y = scaleY2 - scaleY1;
 
         start();
     }
 
-    public void start(final float destX, final float destY) {
+    public void start(final float scaleX, final float scaleY) {
         if (mTarget != null) {
             final PointF scale = mTarget.getScale();
-            start(scale.x, scale.y, destX, destY);
+            start(scale.x, scale.y, scaleX, scaleY);
         }
     }
 
     @Override
     protected void onUpdate(final float value) {
         if (mTarget != null) {
-            mTarget.setScale(mSrcX + value * mDelta.x, mSrcY + value * mDelta.y);
+            mTarget.setScale(mScaleX1 + value * mDelta.x, mScaleY1 + value * mDelta.y);
         }
 
         super.onUpdate(value);
