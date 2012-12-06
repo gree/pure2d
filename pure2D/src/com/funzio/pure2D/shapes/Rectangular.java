f57d06992f521a0bfca74e7ddf949d19f3ec8f35
Long Ngo <long@funzio.com>
2012-12-06 00:48:12 -0800

    optimized to avoid GC

--- pure2D/src/com/funzio/pure2D/shapes/Rectangular.java
+++ pure2D/src/com/funzio/pure2D/shapes/Rectangular.java
@@ -3,6 +3,8 @@
  */
 package com.funzio.pure2D.shapes;
 
+import java.util.Arrays;
+
 import javax.microedition.khronos.opengles.GL10;
 
 import android.graphics.RectF;
@@ -15,9 +17,13 @@ import com.funzio.pure2D.gl.gl10.textures.TextureCoordBuffer;
  */
 public class Rectangular extends Shape {
     private static final int VERTICES_NUM = 4;
+    private float[] mVertices = new float[8];
 
     public Rectangular() {
         super();
+
+        // init vertices
+        Arrays.fill(mVertices, 0);
 
         // use default texture coordinates
         setTextureCoordBuffer(TextureCoordBuffer.getDefault());
@@ -31,17 +37,21 @@ public class Rectangular extends Shape {
     public void setSize(final float w, final float h) {
         super.setSize(w, h);
 
-        final float vertices[] = {
-                0.0f, h, // TL
-                0.0f, 0.0f, // BL
-                w, h, // TR
-                w, 0.0f, // BR
-        };
+        // final float vertices[] = {
+        // 0.0f, h, // TL
+        // 0.0f, 0.0f, // BL
+        // w, h, // TR
+        // w, 0.0f, // BR
+        // };
+        mVertices[1] = h;
+        mVertices[4] = w;
+        mVertices[5] = h;
+        mVertices[6] = w;
 
         if (mVertexBuffer == null) {
-            mVertexBuffer = new VertexBuffer(GL10.GL_TRIANGLE_STRIP, VERTICES_NUM, vertices);
+            mVertexBuffer = new VertexBuffer(GL10.GL_TRIANGLE_STRIP, VERTICES_NUM, mVertices);
         } else {
-            mVertexBuffer.setVertices(GL10.GL_TRIANGLE_STRIP, VERTICES_NUM, vertices);
+            mVertexBuffer.setVertices(GL10.GL_TRIANGLE_STRIP, VERTICES_NUM, mVertices);
         }
     }
 
