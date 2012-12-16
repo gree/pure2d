773991a33b6a1a59bb141d8901959e7c40d5feef
Long Ngo <long@funzio.com>
2012-12-15 18:36:09 -0800

    GL_ARB_texture_non_power_of_two

--- pure2D/src/com/funzio/pure2D/BaseScene.java
+++ pure2D/src/com/funzio/pure2D/BaseScene.java
@@ -188,8 +188,8 @@ public class BaseScene implements Scene {
 
         // find the extension
         mExtensions = gl.glGetString(GL10.GL_EXTENSIONS);
-        Pure2D.NPOT_TEXTURE_SUPPORTED = mNpotTextureSupported = mExtensions.contains("GL_OES_texture_npot");
-        Log.v(TAG, "onSurfaceCreated() | NPOT: " + Pure2D.NPOT_TEXTURE_SUPPORTED);
+        Pure2D.NPOT_TEXTURE_SUPPORTED = mNpotTextureSupported = mExtensions.contains("GL_OES_texture_npot") || mExtensions.contains("GL_ARB_texture_non_power_of_two");
+        Log.v(TAG, "onSurfaceCreated() | NPOT: " + Pure2D.NPOT_TEXTURE_SUPPORTED + "\n" + mExtensions);
 
         // Set the background color to black ( rgba ).
         gl.glClearColor(mColor.r, mColor.g, mColor.b, mColor.a);
