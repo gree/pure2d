6364b94d02b51e08156d66175f49b90a62b78a9d
Sajjad Tabib <sajjad@funzio.com>
2013-03-21 12:00:20 -0700

    added a post task and created a network task

---
+++ pure2D/src/com/funzio/pure2D/loaders/tasks/HttpPostTask.java
@@ -0,0 +1,106 @@
+/**
+ * 
+ */
+package com.funzio.pure2D.loaders.tasks;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+import android.util.Log;
+
+/**
+ * A task that performs a simple HTTP post
+ * @author sajjadtabib
+ * 
+ */
+public class HttpPostTask extends NetworkTask {
+
+    public final String LOG_TAG = HttpPostTask.class.getSimpleName();
+
+    protected String mData;
+    protected Map<String, String> mHttpProperties;
+
+    /**
+     * 
+     */
+    public HttpPostTask(final String data, final Map<String, String> httpProperties) {
+        super();
+        mData = data;
+        mHttpProperties = httpProperties;
+    }
+
+    protected boolean postUrl() {
+        final HttpURLConnection conn;
+
+        try {
+            URL address = new URL(mUrl);
+            conn = (HttpURLConnection) address.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+
+            // add properties to the http post request
+            for (String key : mHttpProperties.keySet()) {
+                String value = mHttpProperties.get(key);
+                if (value != null) {
+                    conn.setRequestProperty(key, value);
+                }
+            }
+
+            conn.setFixedLengthStreamingMode(mData.getBytes().length);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            return false;
+        }
+
+        // now that connection is open send data
+        try {
+            OutputStreamWriter os = new OutputStreamWriter(conn.getOutputStream());
+            os.write(mData);
+            os.close();
+            int responseCode = conn.getResponseCode();
+            if (!(200 <= responseCode) && !(responseCode < 300)) {
+                return false;
+            }
+
+        } catch (IOException e) {
+
+            if (LOG_ENABLED) {
+                Log.v(LOG_TAG, "WRITE ERROR!", e);
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see com.funzio.pure2D.loaders.tasks.NetworkTask#doNetworkTask()
+     */
+    @Override
+    protected boolean doNetworkTask() {
+        return postUrl();
+    }
+
+    /* (non-Javadoc)
+     * @see com.funzio.pure2D.loaders.tasks.NetworkTask#getLogTag()
+     */
+    @Override
+    protected String getLogTag() {
+        return LOG_TAG;
+    }
+
+    /* (non-Javadoc)
+     * @see com.funzio.pure2D.loaders.tasks.Task#reset()
+     */
+    @Override
+    public void reset() {
+        super.reset();
+
+    }
+
+}
--- pure2D/src/com/funzio/pure2D/loaders/tasks/PingTask.java
+++ pure2D/src/com/funzio/pure2D/loaders/tasks/PingTask.java
@@ -16,66 +16,11 @@ import android.util.Log;
  * @author sajjadtabib
  *
  */
-public class PingTask implements Task, Retriable {
+public class PingTask extends NetworkTask implements Retriable {
     private static final String LOG_TAG = PingTask.class.getSimpleName();
-    private static final int DEFAULT_CONNECT_TIMEOUT = 20000;
-    private static final int DEFAULT_READ_TIMEOUT = 20000;
-    private static final int DEFAULT_RETRY_COUNT = 0;
-
-    protected String mUrl;
-    protected boolean mStatus;
-    protected int mMaxRetries;
-    protected int mRetries;
-    protected int mConnectTimeout;
-    protected int mReadTimeout;
-    protected long mBackoffMillis;
 
     public PingTask() {
-        mRetries = 0; //the number of attempted retries
-        mMaxRetries = DEFAULT_RETRY_COUNT;
-        mConnectTimeout = DEFAULT_CONNECT_TIMEOUT;
-        mReadTimeout = DEFAULT_READ_TIMEOUT;
-
-    }
-
-    public PingTask setHttpUrl(final String httpUrl) {
-        mUrl = httpUrl;
-
-        return this;
-    }
-
-    /**
-     * Set the socket connection timeout
-     * @param timeout connection timeout in milliseconds. 0 means no timeout
-     */
-    public PingTask setConnectTimeout(final int timeout) {
-        if (timeout >= 0) {
-            mConnectTimeout = timeout;
-        }
-        return this;
-    }
-
-    /**
-     * Set's the tcp socket read timeout. A value of 0 indicates no timeout
-     * @param timeout
-     */
-    public PingTask setReadTimeout(final int timeout) {
-        if (timeout >= 0) {
-            mReadTimeout = timeout;
-        }
-        return this;
-    }
-
-    public PingTask setRetryCount(final int retry) {
-        if (retry >= 0) {
-            mMaxRetries = retry;
-        }
-        return this;
-    }
-
-    public PingTask setBackoffMillis(final long backoff) {
-        mBackoffMillis = backoff;
-        return this;
+        super();
     }
 
     /* (non-Javadoc)
@@ -83,40 +28,8 @@ public class PingTask implements Task, Retriable {
      */
     @Override
     public void reset() {
-        mStatus = false;
-        mRetries = 0;
+        super.reset();
 
-    }
-
-    /* (non-Javadoc)
-     * @see com.funzio.pure2D.loaders.tasks.Task#run()
-     */
-    @Override
-    public boolean run() {
-
-        Log.d(LOG_TAG, "pinging: " + mUrl);
-        mStatus = ping();
-
-        while (!mStatus && mRetries < mMaxRetries) {
-
-            if (mBackoffMillis > 0) {
-                Log.d(LOG_TAG, "backing off for " + mBackoffMillis + " millis");
-                try {
-                    Thread.sleep(mBackoffMillis);
-                } catch (InterruptedException e) {
-
-                    e.printStackTrace();
-                }
-            }
-
-            //ping the url and get the status
-            Log.d(LOG_TAG, "Retry #: " + (mRetries + 1) + " Max Retries: " + mMaxRetries);
-            mStatus = ping();
-            ++mRetries;
-
-        }
-
-        return mStatus;
     }
 
     protected boolean ping() {
@@ -153,12 +66,21 @@ public class PingTask implements Task, Retriable {
     }
 
     /* (non-Javadoc)
-     * @see com.funzio.pure2D.loaders.tasks.Task#isSucceeded()
+     * @see com.funzio.pure2D.loaders.tasks.NetworkTask#doNetworkTask()
      */
     @Override
-    public boolean isSucceeded() {
-        // TODO Auto-generated method stub
-        return mStatus;
+    protected boolean doNetworkTask() {
+
+        return ping();
+    }
+
+    /* (non-Javadoc)
+     * @see com.funzio.pure2D.loaders.tasks.NetworkTask#getLogTag()
+     */
+    @Override
+    protected String getLogTag() {
+
+        return LOG_TAG;
     }
 
 }
