700ba942af89eeae72b4e57662fbdd2dde4d1187
Long Ngo <long@funzio.com>
2013-08-14 12:36:58 -0700

    more dimensions

--- pure2D/src/com/funzio/pure2D/utils/Dimensions.java
+++ pure2D/src/com/funzio/pure2D/utils/Dimensions.java
@@ -7,11 +7,15 @@ package com.funzio.pure2D.utils;
  * @author long.ngo
  */
 public interface Dimensions {
-    // iOS devices
+    // iphones
     public static final int IPHONE4_WIDTH = 960;
     public static final int IPHONE4_HEIGHT = 640;
     public static final int IPHONE5_WIDTH = 1136;
     public static final int IPHONE5_HEIGHT = IPHONE4_HEIGHT;
+
+    // ipads
+    public static final int IPAD_WIDTH = 1024;
+    public static final int IPAD_HEIGHT = 768;
     public static final int IPAD3_WIDTH = 2048;
     public static final int IPAD3_HEIGHT = 1536;
 }
