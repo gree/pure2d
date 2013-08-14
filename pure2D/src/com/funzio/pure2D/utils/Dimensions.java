b670dc254cdd32bfb38d4ed356206e3a32cceca1
Long Ngo <long@funzio.com>
2013-08-14 11:58:33 -0700

    Dimensions for devices

---
+++ pure2D/src/com/funzio/pure2D/utils/Dimensions.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.funzio.pure2D.utils;
+
+/**
+ * @author long.ngo
+ */
+public interface Dimensions {
+    // iOS devices
+    public static final int IPHONE4_WIDTH = 960;
+    public static final int IPHONE4_HEIGHT = 640;
+    public static final int IPHONE5_WIDTH = 1136;
+    public static final int IPHONE5_HEIGHT = IPHONE4_HEIGHT;
+    public static final int IPAD3_WIDTH = 2048;
+    public static final int IPAD3_HEIGHT = 1536;
+}
