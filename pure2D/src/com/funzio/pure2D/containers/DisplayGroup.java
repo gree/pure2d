3d2a7a2c7684354167fbcf4480d481d792549785
Long Ngo <long@funzio.com>
2013-10-30 17:08:53 -0700

    DisplayGroup.getChildById()

--- pure2D/src/com/funzio/pure2D/containers/DisplayGroup.java
+++ pure2D/src/com/funzio/pure2D/containers/DisplayGroup.java
@@ -478,7 +478,24 @@ public class DisplayGroup extends BaseDisplayObject implements Container, Cachea
     }
 
     public DisplayObject getChildById(final String id) {
-        return mChildrenIds.get(id);
+        DisplayObject child = mChildrenIds.get(id);
+        if (child != null) {
+            return child;
+        }
+
+        // into the grand children
+        for (int i = 0; i < mNumChildren; i++) {
+            child = mChildren.get(i);
+
+            if (child instanceof Container) {
+                final DisplayObject grandChild = ((Container) child).getChildById(id);
+                if (grandChild != null) {
+                    return grandChild;
+                }
+            }
+        }
+
+        return null;
     }
 
     /**
