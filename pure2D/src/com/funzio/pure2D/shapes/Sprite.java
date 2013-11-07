8c7c8dd9af06d4ac10bf5ef6de28ffc8ee881ef3
Long Ngo <long@funzio.com>
2013-11-07 15:22:44 -0800

    added Sprite.setSizeToFrame()

--- pure2D/src/com/funzio/pure2D/shapes/Sprite.java
+++ pure2D/src/com/funzio/pure2D/shapes/Sprite.java
@@ -14,6 +14,7 @@ import com.funzio.pure2D.gl.gl10.textures.TextureCoordBuffer;
  */
 public class Sprite extends Rectangular {
     private boolean mSizeToTexture = true;
+    private boolean mSizeToFrame = true;
 
     protected AtlasFrame mAtlasFrame;
     protected float mOffsetX = 0;
@@ -37,12 +38,25 @@ public class Sprite extends Rectangular {
         return mSizeToTexture;
     }
 
-    public void setSizeToTexture(final boolean value) {
-        mSizeToTexture = value;
+    public void setSizeToTexture(final boolean sizeToTexture) {
+        mSizeToTexture = sizeToTexture;
 
         // fit size to texture
-        if (mTexture != null && mSizeToTexture) {
+        if (mTexture != null && sizeToTexture) {
             setSize(mTexture.getSize());
+        }
+    }
+
+    public boolean isSizeToFrame() {
+        return mSizeToFrame;
+    }
+
+    public void setSizeToFrame(final boolean sizeToFrame) {
+        mSizeToFrame = sizeToFrame;
+
+        // fit size to texture
+        if (mAtlasFrame != null && sizeToFrame) {
+            setSize(mAtlasFrame.getSize());
         }
     }
 
@@ -71,7 +85,7 @@ public class Sprite extends Rectangular {
 
             // size changed?
             final PointF newSize = frame.getSize();
-            if (newSize.x != mSize.x || newSize.y != mSize.y) {
+            if (mSizeToFrame && (newSize.x != mSize.x || newSize.y != mSize.y)) {
                 setSize(newSize.x, newSize.y);
             } else {
                 invalidate(FRAME);
