3f773c3d79202b5775a79fee9da2af4bc9da69ae
longngo78 <me@longngo.com>
2015-07-20 17:40:13 -0700

    added APIs to get sub-frame sets

--- pure2D/src/main/java/com/funzio/pure2D/atlas/JsonAtlas.java
+++ pure2D/src/main/java/com/funzio/pure2D/atlas/JsonAtlas.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
  * Copyright (C) 2012-2014 GREE, Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
  * THE SOFTWARE.
  ******************************************************************************/
 /**
- * 
+ *
  */
 package com.funzio.pure2D.atlas;
 
@@ -83,7 +83,7 @@ public class JsonAtlas extends Atlas {
 
     /**
      * Load from assets
-     * 
+     *
      * @param assets
      * @param filePath
      * @param scale
@@ -107,7 +107,7 @@ public class JsonAtlas extends Atlas {
 
     /**
      * Load from file system
-     * 
+     *
      * @param filePath
      * @param scale
      * @throws IOException
@@ -147,7 +147,7 @@ public class JsonAtlas extends Atlas {
 
     /**
      * Load from a url and cache as an option
-     * 
+     *
      * @param urlPath
      * @param cachePath
      * @param scale
