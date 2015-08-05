f51a4ea4bab12b813e44d541dbd4f86b2fabda55
longngo78 <me@longngo.com>
2015-08-04 19:57:09 -0700

    added TweenAnimatorListener

--- pure2D/src/main/java/com/funzio/pure2D/animators/TweenAnimator.java
+++ pure2D/src/main/java/com/funzio/pure2D/animators/TweenAnimator.java
@@ -1,16 +1,16 @@
-/*******************************************************************************
+/**
  * Copyright (C) 2012-2014 GREE, Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,9 +18,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- ******************************************************************************/
+ */
 /**
- * 
+ *
  */
 package com.funzio.pure2D.animators;
 
@@ -78,7 +78,7 @@ public class TweenAnimator extends BaseAnimator {
                     mTripCount = trips;
                     if (mLoopMode != LoopModes.LOOP_NONE) {
                         // callback
-                        onLoop();
+                        onLoop(trips);
                     }
                 }
 
@@ -152,7 +152,7 @@ public class TweenAnimator extends BaseAnimator {
 
     /**
      * To explicitly play in reversed direction
-     * 
+     *
      * @param reversed
      */
     public void setReversed(final boolean reversed) {
@@ -208,7 +208,13 @@ public class TweenAnimator extends BaseAnimator {
         }
     }
 
-    protected void onLoop() {
-        // TOOD
+    protected void onLoop(final int trips) {
+        if (mListener instanceof TweenAnimatorListener) {
+            ((TweenAnimatorListener) mListener).onLoop(this, trips);
+        }
+    }
+
+    public interface TweenAnimatorListener extends AnimatorListener {
+        void onLoop(final Animator animator, final int trips);
     }
 }
